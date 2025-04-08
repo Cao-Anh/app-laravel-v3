73ecac3bb72f1a96bdaff0321ae1349d6c6e5cff
dangcaoanh <dangcaoanh1998@gmail.com>
2025-04-09 00:46:46 +0700

    fix role

--- database/factories/RoleFactory.php
+++ database/factories/RoleFactory.php
@@ -17,7 +17,7 @@ class RoleFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => $this->faker->jobTitle,
+            'name' => $this->faker->randomElement(['admin', 'manager', 'leader', 'member']),
             'description' => $this->faker->sentence,
         ];
     }
