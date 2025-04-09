85bd9e8c732bf187a51336afb98ea90f0391e24d
dangcaoanh <dangcaoanh1998@gmail.com>
2025-04-10 00:15:54 +0700

    add create store method, routes, page

--- routes/web.php
+++ routes/web.php
@@ -76,11 +76,6 @@ Route::middleware('auth')->group(function () {
     // })->name('dashboard');
     Route::resource('users', UserController::class);
 
-
-    Route::get('/products', [ProductController::class, 'index'])->name('products.index');
-    Route::get('/products/{id}', [ProductController::class, 'show'])->name('products.show');
-    Route::get('/products/{id}/edit', [ProductController::class, 'edit'])->name('products.edit');
-    Route::put('/products/{id}', [ProductController::class, 'update'])->name('products.update');
-    Route::delete('/products/{id}', [ProductController::class, 'destroy'])->name('products.destroy');
-
+    Route::resource('products', ProductController::class);
+ 
 });
