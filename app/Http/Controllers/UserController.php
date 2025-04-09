471affae2a9d7d908cd8104dd4e69c4d3a2ed7c9
dangcaoanh <dangcaoanh1998@gmail.com>
2025-04-09 23:44:38 +0700

    remove image and description fields

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Role;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
-{   
-   
+{
+
     public function index()
     {
         $users = User::paginate(10);
@@ -22,76 +23,82 @@ class UserController extends Controller
     }
 
     public function create()
-{
+    {
+        $roles = Role::all();
+        return view('users.create', compact('roles'));
+    }
 
-    return view('users.create');
-}
 
-public function store(Request $request)
-{
-    $request->validate([
-        'username' => 'required|string|min:3|max:8|unique:users,username',
-        'email' => 'required|email|unique:users,email',
-        'password' => 'required|string|min:6|confirmed',
-        
-    ]);
+    public function store(Request $request)
+    {
+        $request->validate([
+            'username' => 'required|string|min:3|max:8|unique:users,username',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|string|min:6|confirmed',
+            'roles' => 'array|exists:roles,id'
+        ]);
 
-    
-    $user = new User();
-    $user->username = $request->username;
-    $user->email = $request->email;
-    $user->password = bcrypt($request->password); // Never forget to hash passwords!
-    $user->save();
+        $user = new User();
+        $user->username = $request->username;
+        $user->email = $request->email;
+        $user->password = bcrypt($request->password);
+        $user->save();
 
-    return redirect()->route('users.index')->with('success', 'Tạo người dùng thành công.');
-}
+        // Attach roles
+        if ($request->has('roles')) {
+            $user->roles()->attach($request->roles);
+        }
+
+        return redirect()->route('users.index')->with('success', 'Tạo người dùng thành công.');
+    }
+
 
 
     public function edit($id)
-    {   
-        
+    {
+
         $user = User::findOrFail($id);
         return view('users.edit', compact('user'));
     }
 
     public function update(Request $request, $id)
-{
-    $request->validate([
-        'username' => 'required|string|min:3|max:8',
-        'email' => 'required|email|unique:users,email,' . $id,
-        
-    ]);
+    {
+        $request->validate([
+            'username' => 'required|string|min:3|max:8',
+            'email' => 'required|email|unique:users,email,' . $id,
 
-    $user = User::findOrFail($id);
+        ]);
 
-    // if ($request->hasFile('photo')) {
-    //     // dd($request->file('photo'));
-    //     $file = $request->file('photo');
+        $user = User::findOrFail($id);
 
-    //     $imageName = time().'.'.$request->photo->extension();  
+        // if ($request->hasFile('photo')) {
+        //     // dd($request->file('photo'));
+        //     $file = $request->file('photo');
 
-    //     $request->photo->move(public_path('images'), $imageName);
-    //     $photoUrl= 'images/'. $imageName;
-    
-    //     $user->photo = $photoUrl;
-    // }
-    
-    
+        //     $imageName = time().'.'.$request->photo->extension();  
 
-    $user->update([
-        'username' => $request->username,
-        'email' => $request->email,
-    ]);
+        //     $request->photo->move(public_path('images'), $imageName);
+        //     $photoUrl= 'images/'. $imageName;
 
-    $user->save(); 
+        //     $user->photo = $photoUrl;
+        // }
 
-    return redirect()->route('users.show', $id)->with('success', 'Cập nhật thành công.');
-}
+
+
+        $user->update([
+            'username' => $request->username,
+            'email' => $request->email,
+        ]);
+
+        $user->save();
+
+        return redirect()->route('users.show', $id)->with('success', 'Cập nhật thành công.');
+    }
 
 
     public function destroy(Request $request, $id)
-    {   
-       
+    {
+
         $user = User::findOrFail($id);
         $user->delete();
         return redirect()->route('users.index')->with('success', 'Xóa thành công.');
