312657f44ab4bad50a3c7690fde7af536eab8ac6
dangcaoanh <dangcaoanh1998@gmail.com>
2025-04-18 00:17:29 +0700

    add node modules and tailwind

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laravel App</title>
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    @vite('resources/css/app.css')
+
 </head>
 
 <body>
