954bfbbe631e68ac9cfd81f08697e1af35da8d7a
dangcaoanh <dangcaoanh1998@gmail.com>
2025-04-21 02:33:04 +0700

    back to last head

--- app/Http/Requests/UpdateUserRequest.php
+++ app/Http/Requests/UpdateUserRequest.php
@@ -9,11 +9,11 @@ class UpdateUserRequest extends BaseUserRequest
         return $this->user()->can('update', $this->route('user'));
     }
 
-    public function rules(): array
-    {
-        return array_merge($this->baseRules(), [
-            'email' => 'required|email|unique:users,email,' . $this->route('user')->id,
-            'password' => 'sometimes|nullable|string|min:5|max:9|regex:/[A-Z]/|confirmed',
-        ]);
-    }
+    // public function rules(): array
+    // {
+    //     return array_merge($this->baseRules(), [
+    //         'email' => 'required|email|unique:users,email,' . $this->route('user')->id,
+    //         'password' => 'sometimes|nullable|string|min:5|max:9|regex:/[A-Z]/|confirmed',
+    //     ]);
+    // }
 }
